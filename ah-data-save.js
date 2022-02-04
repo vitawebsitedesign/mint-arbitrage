bdbd3d36c3dddef0954a5290116e624379417423
Michael Nguyen <vitawebsitedesign@users.noreply.github.com>
2022-02-05 02:27:30 +1100

    initial commit

---
+++ ah-data-save.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+const getDataCsv = callback => {
+    fs.readFile('ah-data.csv', {encoding: 'utf-8'}, (err, data) => {
+        if (!err) {
+            callback(data);
+        } else {
+            console.log(err);
+        }
+    });
+};
+
+const getDataJson = csv => {
+    const output = {};
+    const lines = csv.trim().split('\n');
+    lines.shift();  // Ignore header line
+    
+    for (let line of lines) {
+        const itemInfo = line.split(',');
+        const price = itemInfo[0];
+        const name = itemInfo[1];
+    
+        // Ignore designs
+        console.log(name);
+        if (name.toLowerCase().startsWith('"design:'))
+            continue;
+    
+        const nameSanitized = name.replaceAll('"', '').replaceAll(' ', '-').toLowerCase();
+        const priceSanitized = (parseFloat(price) / 10000).toString();
+        output[nameSanitized] = priceSanitized;
+    }
+    
+    return JSON.stringify(output);
+};
+
+
+
+const saveDataFile = json => {
+    const unixTime = new Date().getTime().toString();
+    const filename = `${unixTime}.json`;
+    const path = `data/${filename}`;
+    fs.writeFile(path, json, err => {
+        if(err) {
+            return console.error(err);
+        }
+        console.log("The file was saved!");
+    });     
+};
+
+getDataCsv(csv => {
+    const json = getDataJson(csv);
+    saveDataFile(json);
+});
---
+++ arb.js
@@ -0,0 +1,100 @@
+const promisify = require('util').promisify;
+const path = require('path');
+const fs = require('fs');
+const readdirp = promisify(fs.readdir);
+const statp = promisify(fs.stat);
+
+async function getDataFilePaths(directoryName = './data', results = []) {
+    let files = await readdirp(directoryName);
+    files.pop();    // Ignore todays data file - we use that to compare against the averages
+
+    for (let f of files) {
+        let fullPath = path.join(directoryName, f);
+        let stat = await statp(fullPath);
+        if (stat.isDirectory()) {
+            await scan(fullPath, results);
+        } else {
+            results.push(fullPath);
+        }
+    }
+    return results;
+}
+
+async function getDataFilePathLatest(directoryName = './data', results = []) {
+    let files = await readdirp(directoryName);
+    const path = files.pop(); 
+    return `data/${path}`;
+}
+
+async function getJsons(paths) {
+    const jsons = [];
+    for (let path of paths) {
+        const json = await fs.promises.readFile(path, 'utf-8');
+        jsons.push(json);
+    }
+    return jsons;
+}
+
+const getPriceHistory = jsons => {
+    const history = {};
+    for (let json of jsons) {
+        const ahPrices = JSON.parse(json);
+        for (const [name, price] of Object.entries(ahPrices)) {
+            if (name in history) {
+                history[name].push(price);
+            } else {
+                history[name] = [price];
+            }
+        }
+    }
+
+    return history;
+};
+
+const getPriceAverages = history => {
+    const averages = {};
+    for (const [name, prices] of Object.entries(history)) {
+        const sum = prices.map(p => parseFloat(p)).reduce((agg, p) => agg + p, 0);
+        averages[name] = sum / prices.length;
+    }
+
+    return averages;
+};
+
+const getPriceAverageDiff = (historicalAverages, latestAverages) => {
+    const diffs = [];
+    for (const [name, price] of Object.entries(latestAverages)) {
+        const historicalAvg = historicalAverages[name];
+        if (historicalAvg) {
+            const diff = (price - historicalAvg).toFixed(1)
+            diffs.push({
+                name,
+                diff 
+            });
+        }
+    }
+
+    return diffs;
+};
+
+const jsons = [];
+
+(async () => {
+    const paths = await getDataFilePaths();
+    const jsons = await getJsons(paths);
+    const history = getPriceHistory(jsons);
+    const averages = getPriceAverages(history);
+
+    const latestPath = await getDataFilePathLatest();
+    const latestJsons = await getJsons([ latestPath ]);
+    const latestHistory = getPriceHistory(latestJsons);
+    const latestAverages = getPriceAverages(latestHistory);
+
+    const diff = getPriceAverageDiff(averages, latestAverages);
+    const diffOrdered = diff.sort((a, b) => b.diff - a.diff);
+
+    console.log('================')
+    console.log('==== REPORT ====')
+    console.log('================')
+    console.log(diffOrdered);
+})().catch(console.error);
